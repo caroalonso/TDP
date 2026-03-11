538979794085d3b7697ac99d87a71e779ac2caac
Carolina Alonso <103946904+caroalonso@users.noreply.github.com>
2026-03-11 17:51:48 -0300

    Update 6.pas

--- Practicas/Practica 1/Resolución Practica/6.pas
+++ Practicas/Practica 1/Resolución Practica/6.pas
@@ -4,71 +4,54 @@ Procedure busquedaDicotomica (v: vector; ini,fin: indice; dato:integer; var pos:
 Nota: El parámetro “pos” debe retornar la posición del dato o -1 si el dato no se encuentra
 en el vector.}
 
-program punto6;
+program imperativo_seis;
 const
-dimf=5; // dimf original 20
+ dimF=5;
+type 
+ vector=array[1..dimF]of integer;
+ 
+//--------------------------
 
-type
-  rango= 1..dimf;
-  vector=array[rango]of integer;
-
-//PROCESOS
-procedure cargar_vector_total (var v:vector);
+//CARGA DE VECTOR ORDENADA. 
+procedure carga_vector (var v:vector);
 var
-  i:integer;
+i:integer;
 begin
-  for i := 1 to dimf do begin //carga hasta el final del vector 
-    writeln('ingrese un numero de manera ordenada: ');
-    readln (v[i]);
-  end;
+  for i := 1 to dimF do begin
+     v[i]:=i;
+  end;   
 end;
 
-
-procedure busquedaDicotomica (v: vector; pri:integer; ult:integer; dato:integer; var pos: integer); // ini =pri fin=ult.
+//RETORNAR POSICION DE VALOR EN VECTOR DE MANERA RECURSIVA.
+procedure buscar(v:vector; pri,ult:integer; dato:integer; var pos:integer);
 var
   medio:integer;
 begin
-  if(pri > ult)then begin  // se sale del rango.
-    pos:= -1;
-  end
-  else begin  
-    medio:=(pri+ult) div 2;
-    if(v[medio]=dato)then begin
-      pos:=medio;
-    end
-    else begin
-      if(dato<v[medio])then begin
-        ult:= medio -1;
-        busquedaDicotomica(v,pri,ult,dato,pos);
-      end 
-      else begin
-        pri:= medio+1;
-        busquedaDicotomica(v,pri,ult,dato,pos);
-      end;   
-    end; 
-  end;  
+  if(pri>ult) then
+    pos := -1
+  else begin
+    medio := (pri + ult) DIV 2;
+
+    if(v[medio] = dato) then
+      pos := medio
+    else if (v[medio] < dato)then 
+      buscar(v,medio+1,ult,dato,pos)
+    else 
+      buscar(v,pri,medio-1,dato,pos);
+  end;
 end;
 
+//----------------------
 
-//PROGRAMA PRINCIPAL
 var
-  v:vector;
-  dato:integer;
-  pos:integer;
-  pri:integer;
-  ult:integer;
+v:vector;
+valor:integer;
+pos:integer;
 begin
-  cargar_vector_total(v); 
-  pos:=-1;
-  pri:=1;
-  ult:= dimf;
-  writeln('ingrese dato a buscar en el vector : ');
-  read(dato);
-  busquedaDicotomica (v,pri,ult,dato,pos);
-  if(pos=-1)then begin
-    writeln('No se encontro el elemento ', dato ,' en el vector');
-  end
-  else begin
-    writeln('el elemento ',dato,'  se entontro en la posicion ', pos ,' del vector.');
-  end;
+randomize;
+pos:=-1;
+carga_vector(v);
+valor:=random(7)+1;
+buscar(v,1,dimF,valor,pos);
+writeln('posicion de ',valor,': ',pos);
 end.
