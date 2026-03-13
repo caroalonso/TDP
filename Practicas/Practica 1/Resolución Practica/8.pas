45bc80e2e505672a237e47df2ed36b7218eec438
Carolina Alonso <103946904+caroalonso@users.noreply.github.com>
2026-03-13 16:32:08 -0300

    Update 8.pas

--- Practicas/Practica 1/Resolución Practica/8.pas
+++ Practicas/Practica 1/Resolución Practica/8.pas
@@ -1,153 +1,146 @@
 {8. Escribir un programa que:
+
 a. Implemente un módulo que lea números enteros y los almacene en un árbol
 binario de búsqueda.    //   La lectura finaliza con el valor 0.
 
 b. Una vez generado el árbol, realice módulos independientes para:
-1). Obtener el número más grande.
-2) Obtener el número más chico.
-3). Obtener la cantidad de elementos.
-4). Informar los números en orden creciente.
-5). Informar los números pares en orden decreciente.
+1)Obtener el número más grande.
+2)Obtener el número más chico.
+3)Obtener la cantidad de elementos.
+4)Informar los números en orden creciente.
+5)Informar los números pares en orden decreciente.
 
-Nota: Tener en cuenta que cada número debe aparecer una única vez en el árbol.}
+Nota: Tener en cuenta que cada número debe aparecer una única vez en el árbol. // No puede haber repetidos
+}
 
-program punto8;
+
+program imperativo_ocho;
 type
-   arbol=^nodo;
-   nodo=record
-     elem:integer;
-     HD:arbol;  //hijo derecho
-     HI:arbol;  //hijo izquierdo
-   end;  
 
-//PROCESOS
-Procedure crear_arbol (var a:arbol; num:integer); // No se cargan elementos repetidos
-begin
-  if (a=nil)then begin  //caso base
-    new(a);
-    a^.elem:= num;      // crear el primer nodo del arbol
-    a^.HD:=nil;
-    a^.HI:=nil;
-  end
-  else begin 
-    if (num < a^.elem)then begin  // siguientes nodos, rama izquierda (menor).
-      crear_arbol (a^.HI,num);     // manda nodo izq y dato (recursion)
-    end
-    else begin                   // siguientes nodos, rama derecha (mayor)
-      if(num > a^.elem)then begin   
-        crear_arbol (a^.HD,num);   // siguientes nodos, der y dato (recursion)
-      end;  
-    end;
-  end;
-end;
+arbol=^nodo;
 
-
-procedure leer_info(var a:arbol);
-var
-  num:integer;
-begin
-  writeln('ingrese un numero:');
-  read(num);
-  while (num<>0)do begin
-    crear_arbol(a,num);
-    writeln('ingrese un numero:');
-    read(num);
-  end;
+nodo=record
+  elem:integer;
+  sig:arbol;
+  HI:arbol;
+  HD:arbol;
 end;  
 
+//----------------
 
-procedure suma_elementos (a:arbol ; var cant:integer); // PUNTO 3
+
+//CREAR ARBOL SIN REPETIDOS.
+procedure crear(var a:arbol; num:integer);
 begin
-  if (a <> nil)then begin  // caso base
-    cant:=cant+1;                      
-    suma_elementos(a^.HI,cant);
-    suma_elementos(a^.HD,cant);
-  end;
-end; 
+  if(a=nil)then begin
+    new(a);
+    a^.elem:=num;
+    a^.HI:=nil;
+    a^.HD:=nil;
+  end  
+  else
+  begin
+    if(num < a^.elem)then
+       crear(a^.HI,num)
+    else if (num > a^.elem)then // if evita repetidos.
+        crear(a^.HD,num)
+  end;  
+end;
 
 
-procedure Imprimir_arbol_en_orden(a:arbol); //PUNTO 4
+//NUMERO MAXIMO DEL ARBOL.
+function numMax(a:arbol):integer;
 begin
-  if (a <> nil)then begin                        // caso base
-    Imprimir_arbol_en_orden(a^.HI);
-    writeln(a^.elem);
-    Imprimir_arbol_en_orden(a^.HD)  
+  if(a=nil)then //ab vacio
+    numMax:= 9999
+  else 
+    if(a^.HD=nil)then //si mi hijo derecho es nil yo soy el max del arbol.
+      numMax:=a^.elem
+    else
+      numMax:= numMax(a^.HD); //si no sigo bajando por mi hijo derecho.    
+end;
+
+
+//NUMERO MINIMO DEL ARBOL DE MANERA RECURSIVA.
+function numMin(a:arbol):integer;
+begin
+  if(a=nil)then //ab vacio
+    numMin:=-1
+  else
+    if(a^.HI=nil)then //si mi hijo izquiero es nil yo soy el min del arbol.
+      numMin:=a^.elem
+    else
+      numMin:= numMin(a^.HI); //si no sigo bajando por mi hijo izquierdo.      
+end;
+
+
+//CANTIDAD DE ELEMENTOS DEL ARBOL
+procedure cantidadTotal(a:arbol; var cant:integer);
+begin
+  if(a<>nil)then begin
+    cant:=cant+1;
+    cantidadTotal(A^.HI,cant);
+    cantidadTotal(a^.HD,cant);
+  end;    
+end;
+
+
+{
+//CANTIDAD DE ELEMENTOS ARBOL DE MANERA RECURSIVA CON FUNCTION.
+function cantidadTotal(a:arbol):integer;
+begin
+  if(a=nil)then
+    cantidadTotal:= 0
+  else
+    cantidadTotal:= 1 + cantidadTotal(a^.HI) + cantidadTotal(a^.HD);
+end;}
+
+
+
+//INFORMAN NUMEROS EN ORDEN CRECIENTE.
+procedure enOrdenCreciente(a:arbol);
+begin
+  if(a<>nil)then begin
+    enOrdenCreciente(a^.HI);
+    write(a^.elem ,' - ');
+    enOrdenCreciente(a^.HD);
   end;
 end;
 
 
-Procedure imprimir_par (a:arbol);
+//INFORMAN NUMEROS PARES EN ORDEN DECRECIENTE.
+procedure enOrdenDecreciente(a:arbol);
 begin
-  if (a <> nil)then begin  // caso base                  
-     imprimir_par(a^.HD);     //primero lado derecho y despues
-     if (a^.elem mod 2<>0)then begin
-       writeln(a^.elem); 
-     end;  
-     imprimir_par(a^.HI); // lado izquierdo , recorro el arbol completo en orden decreciente.
+  if(a<>nil)then begin
+    enOrdenDecreciente(a^.HD);
+    if(a^.elem mod 2=0)then
+      write(a^.elem ,' - ');
+    enOrdenDecreciente(a^.HI);
   end;
 end;
 
+//----------------
 
-function maximo (a:arbol):integer;  // PUNTO 1
-begin
-  if (a=nil)then begin    //caso base 1 para arbol vacio.
-     maximo:=-1;    // si retorna menos uno el arbol esta vacio.
-  end   
-  else begin
-    if (a^.HD=nil)then begin// caso base 2 se encontro el elemento maximo.
-      maximo:=a^.elem;
-    end  
-    else begin
-        maximo:=maximo (a^.HD);  // sigue buscando por la rama derecha.
-    end;
-  end;
-end;
-
-
-function minimo (a:arbol):integer; // PUNTO 2
-begin
-  if (a=nil)then begin   //caso base 1 para arbol vacio.
-     minimo:=-1;    // si retorna menos uno si el arbol esta vacio.
-  end   
-  else begin
-    if (a^.HI=nil)then begin // caso base 2 se encontro el elemento minimo.
-      minimo:=a^.elem;
-    end  
-    else begin
-        minimo:=minimo(a^.HI);  // sigue buscando por la rama izquierad.
-    end;
-  end;
-end;
-
-
-//PROGRAMA PRINCIPAL
 var
-  abb:arbol;
-  cant:integer;
-  min:integer;
-  max:integer;
+a:arbol;
+num:integer;
+cant:integer;
 begin
-  abb:=nil;
-  cant:=0;  
-  leer_info(abb);
-  max:=maximo(abb); // PUNTO 1
-  min:=minimo(abb); //PUNTO 2
-  if(max = -1 )then begin
-    writeln('el arbol se encuentra vacio');
-  end
-  else begin
-    writeln ('el numero mas grande del arbol es :', max);  
-  end;
-  if(min = -1 )then begin
-    writeln('el arbol se encuentra vacio');
-  end
-  else begin
-    writeln ('el numero mas chico del arbol es :', min);  
-  end;
-  suma_elementos(abb,cant); // PUNTO 3
-  writeln('cantidad de elementos del vector : ',cant);
-  writeln('elementos de menor a mayor del arbol:');
-  Imprimir_arbol_en_orden(abb);  //PUNTO 4
-  writeln('elementos impares del arbol :');
-  imprimir_par(abb); //PUNTO 5
+  randomize;
+  a:=nil;
+  cant:=0;
+  num:=random(30); // 0 a 29
+  while(num<>0)do begin
+    crear(a,num);
+    num:=random(30);
+  end; 
+  writeln('numero maximo del arbol: ', numMax(a));
+  writeln('numero minimo del arbol: ', numMin(a));
+  cantidadTotal(a,cant);
+  writeln('cantidad de elementos del arbol: ',cant);
+  writeln('numeros del arbol en orden creciente:');
+  enOrdenCreciente(a);
+  writeln;
+  writeln('numeros pares del arbol en orden decreciente:');
+  enOrdenDecreciente(a);
 end.
